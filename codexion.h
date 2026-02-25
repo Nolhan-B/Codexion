56b5e54a9ea5fadc735cf17cd46fd7b8b62fe57f
Nolhan-B <nolhanbil@gmail.com>
2026-02-25 17:43:30 +0100

    header done

--- codexion.h
+++ codexion.h
@@ -1,6 +1,13 @@
 #ifndef CODEXION_H
 # define CODEXION_H
 
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
 typedef enum e_scheduler
 {
 	FIFO,
@@ -9,14 +16,46 @@ typedef enum e_scheduler
 
 typedef struct s_config
 {
-	int 		coders_count;
-	int 		tt_burnout;
-	int			tt_compile;
-	int 		tt_debug;
-	int 		tt_refactor;
-	int 		nb_compiles_required;
-	int 		dongle_cooldown;
+	int			nb_coders;
+	int			time_to_burnout;
+	int			time_to_compile;
+	int			time_to_debug;
+	int			time_to_refactor;
+	int			nb_compiles_required;
+	int			dongle_cooldown;
 	t_scheduler	scheduler;
 }	t_config;
+
+typedef struct s_dongle
+{
+	pthread_mutex_t	mutex;
+	pthread_cond_t	cond;
+	long			cooldown_end;
+}	t_dongle;
+
+typedef struct s_coder
+{
+	int				id;
+	pthread_t		thread;
+	t_dongle		*left_dongle;
+	t_dongle		*right_dongle;
+	long			last_compile_start;
+	int				compile_count;
+	struct s_sim	*sim;
+}	t_coder;
+
+typedef struct s_sim
+{
+	t_coder			*coders;
+	t_dongle		*dongles;
+	t_config		config;
+	int				running;
+	pthread_mutex_t	mutex;
+	long			start_time;
+	pthread_mutex_t	log_mutex;
+}	t_sim;
+
+/* Parsing */
+int		parse_args(int ac, char **av, t_config *config);
 
 #endif
